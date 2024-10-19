493b44ba1e049733da3c63704f5e73f8fa7d4d38
parkerkajfasz <parkerkajfasz@gmail.com>
2024-10-18 21:04:25 -0400

    Added API call to ebay query method

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,30 +1,27 @@
 import os
-from flask import Flask, render_template
+import datetime
+from dotenv import load_dotenv
+from ebaysdk.exception import ConnectionError
+from ebaysdk.finding import Connection
+from flask import Flask, render_template, redirect, request, url_for
+from flaskr.ebay import *
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+load_dotenv()
+DEV_KEY = os.getenv('DEV_KEY')
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+app = Flask(__name__)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+ebay = Ebay(DEV_KEY)
 
-    # a simple page that says hello
-    @app.route('/')
-    def hello():
-        return render_template('index.html')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    return app
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    keywords = request.form['keywords']
+    data = ebay.query(keywords)
+    return data
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- flaskr/ebay.py
+++ flaskr/ebay.py
@@ -1,7 +1,44 @@
-class Ebay:
-    def __init__(self):
+import os
+import datetime
+from dotenv import load_dotenv
+from ebaysdk.exception import ConnectionError
+from ebaysdk.finding import Connection
+from flask import Flask, render_template, redirect, request, url_for
+import requests
+from flaskr.ebay import *
+
+class Ebay():
+    def __init__(self, DEV_KEY):
+        self.DEV_KEY = DEV_KEY
         pass
-    def find(self, request):
-        pass
+    def query(self, keywords):
+        try:
+            # api = Connection(appid=self.DEV_KEY, config_file=None)
+            # response = api.execute('findItemsAdvanced', {'keywords': {keywords}, 'categoryId': ['11116']})
+            URL = "https://svcs.ebay.com/services/search/FindingService/v1?"
+            OPERATION = "Operation-Name=findItemsAdvanced"
+            SERVICE_VERSION = "&Service-Version=1.0.0"
+            SECURITY_APPNAME = f"&Security-AppName={self.DEV_KEY}"
+            RESPONSE_FORMAT = "&Response-Data-Format=JSON&REST-Payload"
+            KEYWORDS = "&keywords=Coin%201967%20Canada%201%20Cent&categoryId=11116"
+
+            url = URL + OPERATION + SERVICE_VERSION + SECURITY_APPNAME + RESPONSE_FORMAT + KEYWORDS
+
+            response = requests.get(url)
+
+                # Check if the request was successful
+            if response.status_code == 200:
+                # Process the JSON response
+                data = response.json()
+                print(data)
+                return data
+            else:
+                # Handle errors
+                print(f"Error: {response.status_code}")
+                return None
+                
+        except ConnectionError as e:
+            print(e)
+            print(e.response.dict())
     def parse(self, response):
         pass
